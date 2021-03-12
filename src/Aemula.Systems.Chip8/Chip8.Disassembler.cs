8e629fe02529aad834e134e6987f92ddcd300113
Tim Jones <tim@timjones.io>
2021-03-12 17:30:43 +0000

    Add CHIP-8

---
+++ src/Aemula.Systems.Chip8/Chip8.Disassembler.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections.Generic;
+
+namespace Aemula.Systems.Chip8
+{
+    partial class Chip8
+    {
+        public override SortedDictionary<ushort, DisassembledInstruction> Disassemble()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private DisassembledInstruction Disassemble(ushort address)
+        {
+            var opcodeHi = _memory[address + 0];
+            var opcodeLo = _memory[address + 1];
+            var opcode = (ushort)((opcodeHi << 8) | opcodeLo);
+
+            return new DisassembledInstruction(
+                opcode,
+                address,
+                $"{address:X4}",
+                2,
+                $"{opcodeHi:X2} {opcodeLo:X2}",
+                DisassembleImpl(opcode),
+                null,  // TODO
+                null); // TODO
+        }
+
+        private static string DisassembleImpl(ushort opcode)
+        {
+            var x = (opcode & 0x0F00) >> 8;
+            var y = (opcode & 0x00F0) >> 4;
+
+            return (opcode & 0xF000) switch
+            {
+                0x0000 => opcode switch
+                {
+                    // 00E0 - CLS - Clear the display.
+                    0x00E0 => "CLS",
+
+                    // 00EE - RET - Return from a subroutine.
+                    0x00EE => "RET",
+
+                    _ => "",
+                },
+
+                // 1nnn - JP addr - Jump to location nnn.
+                0x1000 => $"JP #{opcode & 0x0FFF:X4}",
+
+                // 2nnn - CALL addr - Call subroutine at nnn.
+                0x2000 => $"CALL #{opcode & 0x0FFF:X4}",
+
+                // 3xkk - SE Vx, byte - Skip next instruction if Vx = kk.
+                0x3000 => $"SE V{x:X}, #{opcode:X2}",
+
+                // 4xkk - SNE Vx, byte - Skip next instruction if Vx != kk.
+                0x4000 => $"SNE V{x:X}, #{opcode:X2}",
+
+                // 5xy0 - SE Vx, Vy - Skip next instruction if Vx = Vy.
+                0x5000 => $"SE V{x:X}, V{y:X}",
+
+                // 6xkk - LD Vx, byte - Set Vx = kk.
+                0x6000 => $"LD V{x:X}, #{opcode:X2}",
+
+                // 7xkk - ADD Vx, byte - Set Vx = Vx + kk.
+                0x7000 => $"ADD V{x:X}, #{opcode:X2}",
+
+                0x8000 => (opcode & 0x000F) switch
+                {
+                    // 8xy0 - LD Vx, Vy - Set Vx = Vy.
+                    0x0 => $"LD V{x:X}, V{y:X}",
+
+                    // 8xy1 - OR Vx, Vy - Set Vx = Vx OR Vy.
+                    0x1 => $"OR V{x:X}, V{y:X}",
+
+                    // 8xy2 - AND Vx, Vy - Set Vx = Vx AND Vy.
+                    0x2 => $"AND V{x:X}, V{y:X}",
+
+                    // 8xy3 - XOR Vx, Vy - Set Vx = Vx XOR Vy.
+                    0x3 => $"XOR V{x:X}, V{y:X}",
+
+                    // 8xy4 - ADD Vx, Vy - Set Vx = Vx + Vy, set VF = carry.
+                    0x4 => $"ADD V{x:X}, V{y:X}",
+
+                    // 8xy5 - SUB Vx, Vy - Set Vx = Vx - Vy, set VF = NOT borrow.
+                    0x5 => $"SUB V{x:X}, V{y:X}",
+
+                    // 8xy6 - SHR Vx {, Vy} - Set Vx = Vx SHR 1.
+                    0x6 => $"SHR V{x:X}, V{y:X}",
+
+                    // 8xy7 - SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow.
+                    0x7 => $"SUBN V{x:X}, V{y:X}",
+
+                    // 8xyE - SHL Vx {, Vy} - Set Vx = Vx SHL 1.
+                    0xE => $"SHL V{x:X}, V{y:X}",
+
+                    _ => "",
+                },
+
+                // 9xy0 - SNE Vx, Vy - Skip next instruction if Vx != Vy.
+                0x9000 => $"SNE V{x:X}, V{y:X}",
+
+                // Annn - LD I, addr - Set I = nnn.
+                0xA000 => $"LD I, #{opcode & 0xFFF:X4}",
+
+                // Bnnn - JP V0, addr - Jump to location nnn + V0.
+                0xB000 => $"JP V0, #{opcode & 0xFFF:X4}",
+
+                // Cxkk - RND Vx, byte - Set Vx = random byte AND kk.
+                0xC000 => $"RND V{x:X}, #{opcode:X2}",
+
+                // Dxyn - DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I
+                // at (Vx, Vy), set VF = collision.
+                0xD000 => $"DRW V{x:X}, V{y:X}, {opcode & 0x000F}",
+
+                0xE000 => (opcode & 0x00FF) switch
+                {
+                    // Ex9E - SKP Vx - Skip next instruction if key with the value of Vx is pressed.
+                    0x009E => $"SKP V{x:X}",
+
+                    // ExA1 - SKNP Vx - Skip next instruction if key with the value of Vx is not pressed.
+                    0x00A1 => $"SKNP V{x:X}",
+
+                    _ => "",
+                },
+
+                0xF000 => (opcode & 0x00FF) switch
+                {
+                    // Fx07 - LD Vx, DT - Set Vx = delay timer value.
+                    0x0007 => $"LD V{x:X}, DT",
+
+                    // Fx0A - LD Vx, K - Wait for a key press, store the value of the key in Vx.
+                    0x000A => $"LD V{x:X}, K",
+
+                    // Fx15 - LD DT, Vx - Set delay timer = Vx.
+                    0x0015 => $"LD DT, V{x:X}",
+
+                    // Fx18 - LD ST, Vx - Set sound timer = Vx.
+                    0x0018 => $"LD ST, V{x:X}",
+
+                    // Fx1E - ADD I, Vx - Set I = I + Vx.
+                    0x001E => $"ADD I, V{x:X}",
+
+                    // Fx29 - LD F, Vx - Set I = location of sprite for digit Vx.
+                    0x0029 => $"LD F, V{x:X}",
+
+                    // Fx33 - LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2.
+                    0x0033 => $"LD B, V{x:X}",
+
+                    // Fx55 - LD [I], Vx - Stores registers V0 through Vx in memory starting at location I.
+                    0x0055 => $"LD [I] V{x:X}",
+
+                    // Fx65 - LD Vx, [I] - Read registers V0 through Vx from memory starting at location I.
+                    0x0065 => $"LD V{x:X}, [I]",
+
+                    _ => ""
+                },
+
+                _ => ""
+            };
+        }
+    }
+}
---
+++ src/Aemula.Systems.Chip8/Chip8.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.IO;
+
+namespace Aemula.Systems.Chip8
+{
+    public sealed partial class Chip8 : EmulatedSystem
+    {
+        private readonly byte[] _memory; // 4KB
+        private readonly byte[] _v; // V registers
+        private readonly ushort[] _stack;
+        private readonly byte[] _display;
+        private readonly bool[] _keys;
+
+        private readonly Random _random;
+        private readonly byte[] _randomBytes;
+
+        private ushort _i;
+        private ushort _pc;
+        private byte _sp;
+
+        private byte _dt; // Delay timer
+        private byte _st; // Sound timer
+
+        private bool _waitingForKeyPress;
+        private byte _waitingForKeyPressRegister;
+
+        private bool _running;
+
+        public override ushort LastPC => throw new NotImplementedException();
+
+        public Chip8()
+        {
+            _memory = new byte[0x1000];
+            _v = new byte[16];
+            _stack = new ushort[16];
+            _display = new byte[ScreenWidth * ScreenHeight];
+            _keys = new bool[16];
+            _random = new Random(42);
+            _randomBytes = new byte[1];
+
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            Array.Clear(_memory, 0, _memory.Length);
+            Array.Clear(_v, 0, _v.Length);
+            Array.Clear(_stack, 0, _stack.Length);
+            Array.Clear(_display, 0, _display.Length);
+            Array.Clear(_keys, 0, _keys.Length);
+            _i = 0;
+            _pc = ProgramStart;
+            _sp = 0;
+            _dt = 0;
+            _st = 0;
+            _waitingForKeyPress = false;
+            _waitingForKeyPressRegister = 0;
+
+            // Add sprite characters at start of memory.
+            Array.Copy(SpriteCharacters, _memory, SpriteCharacters.Length);
+
+            _running = false;
+        }
+
+        public override void LoadProgram(string filePath)
+        {
+            var program = File.ReadAllBytes(filePath);
+            Array.Copy(program, 0, _memory, ProgramStart, program.Length);
+        }
+
+        public override void RunForDuration(TimeSpan duration)
+        {
+            // We need to run at ~600Hz.
+            var numCycles = (int)Math.Round(duration.TotalSeconds * 600.0);
+            for (var i = 0; i < numCycles; i++)
+            {
+                Cycle();
+            }
+
+            UpdateTimers();
+        }
+
+        public override void StepInstruction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StepCpuCycle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateTimers()
+        {
+            if (_dt > 0)
+            {
+                _dt -= 1;
+            }
+
+            if (_st > 0)
+            {
+                // TODO: Play sound
+                _st -= 1;
+            }
+        }
+
+        public void SetKeyDown(byte key)
+        {
+            _keys[key] = true;
+
+            if (_waitingForKeyPress)
+            {
+                _v[_waitingForKeyPressRegister] = key;
+                _waitingForKeyPress = false;
+            }
+        }
+
+        public void SetKeyUp(byte key)
+        {
+            _keys[key] = false;
+        }
+
+        private void Cycle()
+        {
+            if (_waitingForKeyPress)
+            {
+                return;
+            }
+
+            var kk = _memory[_pc + 1];
+            var opcode = (ushort)((_memory[_pc] << 8) | kk);
+
+            _pc += 2;
+
+            var x = (opcode & 0x0F00) >> 8;
+            var y = (opcode & 0x00F0) >> 8;
+            var nnn = (ushort)(opcode & 0xFFF);
+
+            switch (opcode & 0xF000)
+            {
+                case 0x0000:
+                    switch (opcode)
+                    {
+                        // CLS - Clear the display.
+                        case 0x00E0:
+                            Array.Clear(_display, 0, _display.Length);
+                            break;
+
+                        // RET - Return from a subroutine.
+                        case 0x00EE:
+                            _sp--;
+                            _pc = _stack[_sp];
+                            break;
+                    }
+                    break;
+
+                // 1nnn - JP addr - Jump to location nnn.
+                case 0x1000:
+                    _pc = nnn;
+                    break;
+
+                // 2nnn - CALL addr - Call subroutine at nnn.
+                case 0x2000:
+                    _stack[_sp] = _pc;
+                    _sp++;
+                    _pc = nnn;
+                    break;
+
+                // 3xkk - SE Vx, byte - Skip next instruction if Vx = kk.
+                case 0x3000:
+                    if (_v[x] == kk)
+                    {
+                        _pc += 2;
+                    }
+                    break;
+
+                // 4xkk - SNE Vx, byte - Skip next instruction if Vx != kk.
+                case 0x4000:
+                    if (_v[x] != kk)
+                    {
+                        _pc += 2;
+                    }
+                    break;
+
+                // 5xy0 - SE Vx, Vy - Skip next instruction if Vx = Vy.
+                case 0x5000:
+                    if (_v[x] == _v[y])
+                    {
+                        _pc += 2;
+                    }
+                    break;
+
+                // 6xkk - LD Vx, byte - Set Vx = kk.
+                case 0x6000:
+                    _v[x] = kk;
+                    break;
+
+                // 7xkk - ADD Vx, byte - Set Vx = Vx + kk.
+                case 0x7000:
+                    _v[x] += kk;
+                    break;
+
+                case 0x8000:
+                    switch (opcode & 0x000F)
+                    {
+                        // 8xy0 - LD Vx, Vy - Set Vx = Vy.
+                        case 0x0:
+                            _v[x] = _v[y];
+                            break;
+
+                        // 8xy1 - OR Vx, Vy - Set Vx = Vx OR Vy.
+                        case 0x1:
+                            _v[x] |= _v[y];
+                            break;
+
+                        // 8xy2 - AND Vx, Vy - Set Vx = Vx AND Vy.
+                        case 0x2:
+                            _v[x] &= _v[y];
+                            break;
+
+                        // 8xy3 - XOR Vx, Vy - Set Vx = Vx XOR Vy.
+                        case 0x3:
+                            _v[x] ^= _v[y];
+                            break;
+
+                        // 8xy4 - ADD Vx, Vy - Set Vx = Vx + Vy, set VF = carry.
+                        case 0x4:
+                            (_v[x], _v[0xF]) = OverflowingAdd(_v[x], _v[y]);
+                            break;
+
+                        // 8xy5 - SUB Vx, Vy - Set Vx = Vx - Vy, set VF = NOT borrow.
+                        case 0x5:
+                            (_v[x], _v[0xF]) = OverflowingSub(_v[x], _v[y]);
+                            break;
+
+                        // 8xy6 - SHR Vx {, Vy} - Set Vx = Vx SHR 1.
+                        case 0x6:
+                            (_v[x], _v[0xF]) = OverflowingShr(_v[x]);
+                            break;
+
+                        // 8xy6 - SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow.
+                        case 0x7:
+                            (_v[x], _v[0xF]) = OverflowingSub(_v[y], _v[x]);
+                            break;
+
+                        // 8xyE - SHL Vx {, Vy} - Set Vx = Vx SHL 1.
+                        case 0xE:
+                            (_v[x], _v[0xF]) = OverflowingShl(_v[x]);
+                            break;
+                    }
+                    break;
+
+                // 9xy0 - SNE Vx, Vy - Skip next instruction if Vx != Vy.
+                case 0x9000:
+                    if (_v[x] != _v[y])
+                    {
+                        _pc += 2;
+                    }
+                    break;
+
+                // Annn - LD I, addr - Set I = nnn.
+                case 0xA000: 
+                    _i = nnn;
+                    break;
+
+                // Bnnn - JP V0, addr - Jump to location nnn + V0.
+                case 0xB000:
+                    _pc = (ushort)(nnn + _v[0]);
+                    break;
+
+                // Cxkk - RND Vx, byte - Set Vx = random byte AND kk.
+                case 0xC000:
+                    _random.NextBytes(_randomBytes);
+                    _v[x] = (byte)(_randomBytes[0] & kk);
+                    break;
+
+                // Dxyn - DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I
+                // at (Vx, Vy), set Vf = collision.
+                case 0xD000:
+                    var height = opcode & 0x000F;
+                    var vx = _v[x];
+                    var vy = _v[y];
+                    _v[0xF] = 0;
+                    for (var pixelY = 0; pixelY < height; pixelY++)
+                    {
+                        var sprite = _memory[_i + pixelY];
+                        var actualY = (vx + pixelY) % ScreenHeight;
+                        for (var pixelX = 0; pixelX < 8; pixelX++)
+                        {
+                            if ((sprite & 0x80) != 0)
+                            {
+                                var actualX = (vx + pixelX) % ScreenWidth;
+                                var displayIndex = (actualY * ScreenWidth) + actualX;
+                                _display[displayIndex] ^= 1;
+                                if (_display[displayIndex] == 0)
+                                {
+                                    _v[0xF] = 1;
+                                }
+                            }
+                            sprite <<= 1;
+                        }
+                    }
+                    break;
+
+                case 0xE000:
+                    switch (opcode & 0x00FF)
+                    {
+                        // Ex9E - SKP Vx - Skip next instruction if key with the value of Vx is pressed.
+                        case 0x009E:
+                            if (_keys[_v[x]])
+                            {
+                                _pc += 2;
+                            }
+                            break;
+
+                        // ExA1 - SKNP Vx - Skip next instruction if key with the value of Vx is not pressed.
+                        case 0x00A1:
+                            if (!_keys[_v[x]])
+                            {
+                                _pc += 2;
+                            }
+                            break;
+                    }
+                    break;
+
+                case 0xF000:
+                    switch (opcode & 0x00FF)
+                    {
+                        // Fx07 - LD Vx, DT - Set Vx = delay timer value.
+                        case 0x0007:
+                            _v[x] = _dt;
+                            break;
+
+                        // Fx0A - LD Vx, K - Wait for a key press, store the value of the key in Vx.
+                        case 0x000A:
+                            _waitingForKeyPress = true;
+                            _waitingForKeyPressRegister = (byte)x;
+                            break;
+
+                        // Fx15 - LD DT, Vx - Set delay timer = Vx.
+                        case 0x0015:
+                            _dt = _v[x];
+                            break;
+
+                        // Fx18 - LD ST, Vx - Set sound timer = Vx.
+                        case 0x0018:
+                            _st = _v[x];
+                            break;
+
+                        // Fx1E - ADD I, Vx - Set I = I + Vx.
+                        case 0x001E:
+                            _i += _v[x];
+                            break;
+
+                        // Fx29 - LD F, Vx - Set I = location of sprite for digit Vx.
+                        case 0x0029:
+                            _i = (ushort)(_v[x] * SpriteCharacterHeight);
+                            break;
+
+                        // Fx33 - LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2
+                        case 0x0033:
+                            var value = _v[x];
+                            for (var i = 2; i >= 0; i--)
+                            {
+                                _memory[_i + i] = (byte)(value % 10);
+                                value /= 10;
+                            }
+                            break;
+
+                        // Fx55 - LD [I], Vx - Stores registers V0 through Vx in memory starting at location I.
+                        case 0x0055:
+                            Array.Copy(_v, 0, _memory, _i, x + 1);
+                            break;
+
+                        // Fx65 - LD Vx, [I] - Read registers V0 through Vx from memory starting at location I.
+                        case 0x0065:
+                            Array.Copy(_memory, _i, _v, 0, x + 1);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private const byte Zero = 0;
+        private const byte One = 1;
+
+        private static (byte result, byte vf) OverflowingAdd(byte x, byte y)
+        {
+            var temp = x + y;
+            return ((byte)temp, (temp > byte.MaxValue) ? One : Zero);
+        }
+
+        private static (byte result, byte vf) OverflowingSub(byte x, byte y)
+        {
+            var temp = x - y;
+            return ((byte)temp, (temp >= 0) ? One : Zero);
+        }
+
+        private static (byte result, byte vf) OverflowingShr(byte x)
+        {
+            return ((byte)(x >> 1), ((x & 1) != 0) ? One : Zero);
+        }
+
+        private static (byte result, byte vf) OverflowingShl(byte x)
+        {
+            return ((byte)(x << 1), ((x & 0x80) != 0) ? One : Zero);
+        }
+    }
+}
